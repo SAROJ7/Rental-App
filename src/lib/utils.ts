f9d704669dd9a505f3a2ac3fc27f0aec3f17407d
saroj7 <sarojsht377@gmail.com>
2025-02-26 00:27:44 +0545

    mod: utils file

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { toast } from "sonner";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatEnumString(str: string) {
+  return str.replace(/([A-Z])/g, " $1").trim();
 }
