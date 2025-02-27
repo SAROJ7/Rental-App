0ec7d85c68be41a0856fd6d90f126be3b3a73765
SAROJ7 <32197983+SAROJ7@users.noreply.github.com>
2025-02-27 15:26:51 +0545

    Landing page (#2)
    * add: Hero Section and Feature Section
    
    * add: Discover Section
    
    * add: Call To Action Section
    
    * add: fortawesome packages
    
    * add: footer section

---
+++ src/types/landingPage.ts
@@ -0,0 +1,13 @@
+export interface IFeatureCard {
+  imageSrc: string;
+  title: string;
+  description: string;
+  linkText: string;
+  linkHref: string;
+}
+
+export interface IDiscoverCard {
+  imageSrc: string;
+  title: string;
+  description: string;
+}
