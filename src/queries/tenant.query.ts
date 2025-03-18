06a717532af20e8983809dcfc13567054489385e
saroj7 <sarojsht377@gmail.com>
2025-03-19 00:07:03 +0545

    mod: Tenant Query to query validate auth

--- src/queries/tenant.query.ts
+++ src/queries/tenant.query.ts
@@ -1,10 +1,18 @@
 import { updateTenantSettings } from "@/apis";
 import { TAGS } from "@/constants";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
 export const useUpdateTenantSettingsMutation = () => {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationKey: [TAGS.UPDATE_TENANT_SETTINGS],
     mutationFn: updateTenantSettings,
+    onSuccess: () => {
+      toast.success("Updated Successfully");
+      queryClient.invalidateQueries({
+        queryKey: [TAGS.GET_AUTH_USER],
+      });
+    },
   });
 };
