cdc6c3a429f98384ac89ffc6fe30b2b67f498f7a
saroj7 <sarojsht377@gmail.com>
2025-03-18 17:39:26 +0545

    add: Settings Form components

--- src/types/common.ts
+++ src/types/common.ts
@@ -1,8 +1,16 @@
+import { SettingsFormData } from "@/lib";
+
 export interface LayoutProps
   extends Readonly<{
     children: React.ReactNode;
   }> {}
 
 export interface AppSidebarProps {
+  userType: "manager" | "tenant";
+}
+
+export interface SettingsFormProps {
+  initialData: SettingsFormData;
+  onSubmit: (data: SettingsFormData) => Promise<void>;
   userType: "manager" | "tenant";
 }
