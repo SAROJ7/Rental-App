7b6df1a1c7a5d6e688f540ca2da8e986923719ee
saroj7 <sarojsht377@gmail.com>
2025-03-18 17:38:29 +0545

    add: Tenant and Manager Settings Page

---
+++ src/app/(dashboard)/managers/settings/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+import SettingsForm from "@/components/SettingsForm";
+import { useGetAuth } from "@/queries/auth.query";
+import { useUpdateManagerSettingsMutation } from "@/queries/manager.query";
+import React from "react";
+
+const ManagerSettings = () => {
+  const { data: authUserInfo, isLoading } = useGetAuth();
+  const authUser = authUserInfo?.data;
+  const { mutateAsync: updateManagerSettings } =
+    useUpdateManagerSettingsMutation();
+
+  if (isLoading) return <>Loading...</>;
+
+  const initialData = {
+    name: authUser?.userInfo?.name,
+    email: authUser?.userInfo?.email,
+    phoneNumber: authUser?.userInfo?.phoneNumber,
+  };
+
+  const handleSubmit = async (data: typeof initialData) => {
+    await updateManagerSettings({
+      cognitoId: authUser?.cognitoInfo?.userId,
+      updateManager: data,
+    });
+  };
+  return (
+    <SettingsForm
+      initialData={initialData}
+      onSubmit={handleSubmit}
+      userType="manager"
+    />
+  );
+};
+
+export default ManagerSettings;
