23aab5726f55c07a296f6dd91bfdd9b00038a1ae
saroj7 <sarojsht377@gmail.com>
2025-03-25 22:24:58 +0545

    mod: Global CSS to support Radar Map

--- src/app/(nondashboard)/search/Map.tsx
+++ src/app/(nondashboard)/search/Map.tsx
@@ -3,14 +3,13 @@ import React, { useEffect, useRef } from "react";
 
 // Import Radar from your installed package
 import Radar from "radar-sdk-js";
-import "radar-sdk-js/dist/radar.css";
+import "maplibre-gl/dist/maplibre-gl.css";
 import { useGlobalStore } from "@/store/global.store";
 import { useGetPropertiesQuery } from "@/queries/property.query";
 
 const Map = () => {
   const mapContainerRef = useRef<HTMLDivElement>(null);
   const filters = useGlobalStore((state) => state.filters);
-  const isFiltersFullOpen = useGlobalStore((state) => state.isFiltersFullOpen);
 
   const {
     data: properties,
@@ -24,7 +23,7 @@ const Map = () => {
     Radar.initialize(process.env.NEXT_PUBLIC_RADAR_API_KEY as string);
 
     const map = Radar.ui.map({
-      container: mapContainerRef.current,
+      container: mapContainerRef.current!,
       style: "radar-default-v1",
       center: filters.coordinates || [-74.5, 40],
       zoom: 9,
@@ -33,31 +32,39 @@ const Map = () => {
     // Add markers for each property
     properties.forEach((property) => {
       const marker = Radar.ui
-        .marker({ color: "#000000" })
+        .marker({
+          color: "#000000",
+          opacityWhenCovered: "1",
+        })
         .setLngLat([
           property.location.coordinates.longitude,
           property.location.coordinates.latitude,
         ])
+        .setPopup(
+          Radar.ui
+            .popup({
+              closeButton: false,
+            })
+            .setHTML(
+              `
+        <div class="marker-popup">
+          <div class="marker-popup-image"></div>
+          <div>
+            <a href="/search/${property.id}" target="_blank" class="marker-popup-title">${property.name}</a>
+            <p class="marker-popup-price">
+              $${property.pricePerMonth}
+              <span class="marker-popup-price-unit"> / month</span>
+            </p>
+          </div>
+        </div>
+        `
+            )
+        )
         .addTo(map);
 
       // Create popup for the marker
-      const popupContent = document.createElement("div");
-      popupContent.className = "marker-popup";
-      popupContent.innerHTML = `
-        <div class="marker-popup-image"></div>
-        <div>
-          <a href="/search/${property.id}" target="_blank" class="marker-popup-title">${property.name}</a>
-          <p class="marker-popup-price">
-            $${property.pricePerMonth}
-            <span class="marker-popup-price-unit"> / month</span>
-          </p>
-        </div>
-      `;
 
-      // Add popup to marker
-      const popup = Radar.ui.popup({}).setDOMContent(popupContent);
-
-      marker.setPopup(popup);
+      // Add popup to marker with closeButton set to false
     });
 
     // Handle resize event
@@ -67,7 +74,7 @@ const Map = () => {
     resizeMap();
 
     return () => map.remove();
-  });
+  }, [isLoading, isError, properties, filters.coordinates]);
 
   return (
     <div className="basis-5/12 grow relative rounded-xl">
