37f48e795289f1c763bfafb3aec9fae1a6f144c8
saroj7 <sarojsht377@gmail.com>
2025-03-25 22:25:09 +0545

    Add: Listing Page

---
+++ src/app/(nondashboard)/search/Listings.tsx
@@ -0,0 +1,84 @@
+import Card from "@/components/Card";
+import { useGetAuth } from "@/queries/auth.query";
+import { useGetPropertiesQuery } from "@/queries/property.query";
+import {
+  useAddFavoritePropertyMutation,
+  useRemoveFavoritePropertyMutation,
+} from "@/queries/tenant.query";
+import { useGlobalStore } from "@/store/global.store";
+import { Property } from "@/types/prismaTypes";
+import React from "react";
+
+const Listings = () => {
+  const { data: authUser } = useGetAuth();
+  const { mutateAsync: addFavoriteProperty } = useAddFavoritePropertyMutation();
+  const { mutateAsync: removeFavoriteProperty } =
+    useRemoveFavoritePropertyMutation();
+  const viewMode = useGlobalStore((state) => state.viewMode);
+  const filters = useGlobalStore((state) => state.filters);
+
+  const {
+    data: properties,
+    isLoading,
+    isError,
+  } = useGetPropertiesQuery(filters);
+  console.log({ properties });
+
+  const handleFavoriteToggle = async (propertyId: number) => {
+    if (!authUser) return;
+
+    const isFavorite = authUser.data?.userInfo.favorites.some(
+      (fav: Property) => fav.id === propertyId
+    );
+
+    if (isFavorite) {
+      await removeFavoriteProperty({
+        cognitoId: authUser.data?.cognitoInfo.userId,
+        propertyId,
+      });
+    } else {
+      await addFavoriteProperty({
+        cognitoId: authUser.data?.cognitoInfo.userId,
+        propertyId,
+      });
+    }
+  };
+
+  if (isLoading) return <div>Loading...</div>;
+  if (isError || !properties) return <div>Failed to fetch properties</div>;
+
+  return (
+    <div className="w-full">
+      <h3 className="text-sm font-bold px-4">
+        {properties.length}{" "}
+        <span className="text-gray-700 font-normal">
+          Places in {filters.location}
+        </span>
+      </h3>
+      <div className="flex">
+        <div className="p-4 w-full">
+          {properties.map((property) =>
+            viewMode === "grid" ? (
+              <Card
+                key={property.id}
+                property={property}
+                isFavorite={
+                  authUser?.data?.userInfo?.favorites?.some(
+                    (fav: Property) => fav.id === property.id
+                  ) || false
+                }
+                onFavoriteToggle={() => handleFavoriteToggle(property.id)}
+                showFavoriteButton={!!authUser?.data}
+                propertyLink={`/search/${property.id}`}
+              />
+            ) : (
+              <React.Fragment key={property.id}>Another Card</React.Fragment>
+            )
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Listings;
--- src/types/common.ts
+++ src/types/common.ts
@@ -1,4 +1,5 @@
 import { SettingsFormData } from "@/lib";
+import { Property } from "./prismaTypes";
 
 export interface LayoutProps
   extends Readonly<{
@@ -13,4 +14,12 @@ export interface SettingsFormProps {
   initialData: SettingsFormData;
   onSubmit: (data: SettingsFormData) => Promise<void>;
   userType: "manager" | "tenant";
+}
+
+export interface CardProps {
+  property: Property;
+  isFavorite: boolean;
+  onFavoriteToggle: () => void;
+  showFavoriteButton?: boolean;
+  propertyLink?: string;
 }
