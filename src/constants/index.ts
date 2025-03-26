7d588f0c9b235eab0148c934634abb9c66320ff9
saroj7 <sarojsht377@gmail.com>
2025-03-26 14:49:11 +0545

    mod: Added Get Tenant API

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,6 +5,7 @@ export const TAGS = {
   UPDATE_TENANT_SETTINGS: "update-tenant-settings",
   ADD_FAVORITE_PROPERTY: "add-favorite-property",
   REMOVE_FAVORITE_PROPERTY: "remove-favorite-property",
+  GET_TENANT: "get-tenant",
 
   //Managers
   UPDATE_Manager_SETTINGS: "update-manager-settings",
