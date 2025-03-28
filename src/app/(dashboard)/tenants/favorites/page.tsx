938e6c8461fd9a8cbb634b74b15e4d6b1ea56b7c
saroj7 <sarojsht377@gmail.com>
2025-03-28 23:18:23 +0545

    add: Favorite Page

--- src/app/(dashboard)/tenants/favorites/page.tsx
+++ src/app/(dashboard)/tenants/favorites/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+import Card from "@/components/Card";
+import Header from "@/components/Header";
 import Loading from "@/components/Loading";
 import { useGetAuth } from "@/queries/auth.query";
 import { useGetPropertiesQuery } from "@/queries/property.query";
@@ -30,8 +32,31 @@ const Favorites = () => {
   );
 
   if (isLoading) return <Loading />;
+  if (isError) return <div>Error Loading Favorites</div>;
 
-  return <div>Favorites</div>;
+  return (
+    <div className="dashboard-container">
+      <Header
+        title="Favorited Properties"
+        subtitle="Browse and Manage your saved property listings"
+      />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        {favoriteProperties.map((property) => (
+          <Card
+            key={property.id}
+            property={property}
+            isFavorite={true}
+            onFavoriteToggle={() => {}}
+            showFavoriteButton={false}
+            propertyLink={`/tenants/residences/${property.id}`}
+          />
+        ))}
+      </div>
+      {(!favoriteProperties || favoriteProperties.length === 0) && (
+        <p>You don&pos;t have any favorited properties</p>
+      )}
+    </div>
+  );
 };
 
 export default Favorites;
