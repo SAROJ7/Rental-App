7429329d6d3dce547faf12169a3d2751723329d3
saroj7 <sarojsht377@gmail.com>
2025-03-31 15:58:39 +0545

    mod: Add Get Property Lease Query

--- src/apis/lease.api.ts
+++ src/apis/lease.api.ts
@@ -10,3 +10,10 @@ export const getPayments = async (leaseId: number): Promise<Payment[]> => {
   const response = await axiosInstance.get(`/leases/${leaseId}/payments`);
   return response.data;
 };
+
+export const getPropertyLeases = async (
+  propertyId: number
+): Promise<Lease[]> => {
+  const response = await axiosInstance.get(`property/${propertyId}/leases`);
+  return response.data;
+};
